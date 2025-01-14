4776a7b56ec85aaf81b156eacee27ed4d23b4d46
joyeajin <dpwls31200@gmail.com>
2025-01-14 17:31:57 +0900

    init

--- src/atom.tsx
+++ src/atom.tsx
@@ -1,36 +1,6 @@
 import { atom, selector } from "recoil";
-import { recoilPersist } from "recoil-persist";
 
-export enum Categories {
-  "TO_DO" = "TO_DO",
-  "DOING" = "DOING",
-  "DONE" = "DONE",
-}
-
-export interface IToDo {
-  text: string;
-  category: Categories;
-  id: number;
-}
-
-export const categoryState = atom<Categories>({
-  key: "category",
-  default: Categories.TO_DO,
-});
-
-const { persistAtom } = recoilPersist();
-
-export const toDoState = atom<IToDo[]>({
-  key: "toDo",
-  default: [],
-  effects_UNSTABLE: [persistAtom],
-});
-
-export const toDoSelector = selector({
-  key: "toDoSelector",
-  get: ({ get }) => {
-    const toDos = get(toDoState);
-    const category = get(categoryState);
-    return toDos.filter((toDo) => toDo.category === category);
-  },
+export const minuteState = atom({
+  key: "minutes",
+  default: 0,
 });
