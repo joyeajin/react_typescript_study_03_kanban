465679ea58e772c62bff94566f9e832c9da11715
joyeajin <dpwls31200@gmail.com>
2025-01-14 16:43:41 +0900

    enum

--- src/atom.tsx
+++ src/atom.tsx
@@ -1,14 +1,20 @@
 import { atom, selector } from "recoil";
 
+export enum Categories {
+  "TO_DO" = "TO_DO",
+  "DOING" = "DOING",
+  "DONE" = "DONE",
+}
+
 export interface IToDo {
   text: string;
-  category: "DONE" | "DOING" | "TO_DO";
+  category: Categories;
   id: number;
 }
 
-export const categoryState = atom({
+export const categoryState = atom<Categories>({
   key: "category",
-  default: "TO_DO",
+  default: Categories.TO_DO,
 });
 
 export const toDoState = atom<IToDo[]>({
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,24 +1,25 @@
 import { useRecoilState, useRecoilValue } from "recoil";
 import CreateToDo from "./CreateToDo";
-import { categoryState, toDoSelector, toDoState } from "../atom";
+import { Categories, categoryState, toDoSelector } from "../atom";
 import ToDo from "./Todo";
 
 function ToDoList() {
   const toDos = useRecoilValue(toDoSelector);
   const [category, setCategory] = useRecoilState(categoryState);
   const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
-    setCategory(event.currentTarget.value);
+    setCategory(event.currentTarget.value as any);
   };
-  console.log(category);
+
+  console.log(toDos);
 
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
       <select value={category} onInput={onInput}>
-        <option value="TO_DO">To Do</option>
-        <option value="DOING">Doing</option>
-        <option value="DONE">Done</option>
+        <option value={Categories.TO_DO}>To Do</option>
+        <option value={Categories.DOING}>Doing</option>
+        <option value={Categories.DONE}>Done</option>
       </select>
       <CreateToDo />
       {toDos?.map((toDo) => (
