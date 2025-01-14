4546812b341e74c47c34ea4a2d85e350a87bffc8
joyeajin <dpwls31200@gmail.com>
2025-01-14 17:12:09 +0900

    recoil-persist 사용해서 로컬스토리지에 저장

--- src/atom.tsx
+++ src/atom.tsx
@@ -1,4 +1,5 @@
 import { atom, selector } from "recoil";
+import { recoilPersist } from "recoil-persist";
 
 export enum Categories {
   "TO_DO" = "TO_DO",
@@ -17,9 +18,12 @@ export const categoryState = atom<Categories>({
   default: Categories.TO_DO,
 });
 
+const { persistAtom } = recoilPersist();
+
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: [],
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const toDoSelector = selector({
