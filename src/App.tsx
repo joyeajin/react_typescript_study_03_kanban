32795d330494b94494e29f6dc2bf05adc5e0690a
joyeajin <dpwls31200@gmail.com>
2025-01-15 14:27:59 +0900

    Object.keys(obj)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-import {
-  DragDropContext,
-  Draggable,
-  Droppable,
-  DropResult,
-} from "react-beautiful-dnd";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { toDoState } from "./atom";
-import DraggableCard from "./Components/DraggableCard";
+import Board from "./Components/Board";
 
 const Wrapper = styled.div`
   display: flex;
-  max-width: 480px;
+  max-width: 680px;
   width: 100%;
   margin: 0 auto;
   justify-content: center;
@@ -19,18 +14,11 @@ const Wrapper = styled.div`
   height: 100vh;
 `;
 
-const Board = styled.div`
-  background-color: ${(props) => props.theme.boardColor};
-  padding: 20px 10px;
-  padding-top: 30px;
-  border-radius: 5px;
-  min-height: 200px;
-`;
-
 const Boards = styled.div`
   display: grid;
-  grid-template-columns: repeat(1, 1fr);
+  grid-template-columns: repeat(3, 1fr);
   width: 100%;
+  gap: 10px;
 `;
 
 function App() {
@@ -38,35 +26,21 @@ function App() {
   const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
     if (!destination) return;
 
-    setToDos((oldToDos) => {
-      const toDosCopy = [...oldToDos];
-      // console.log("Delete item on", source.index);
-      // console.log(toDosCopy);
+    // setToDos((oldToDos) => {
+    //   const toDosCopy = [...oldToDos];
 
-      toDosCopy.splice(source.index, 1);
-      // console.log("Delete item");
-      // console.log(toDosCopy);
-
-      // console.log("Put back", draggableId, "on", destination.index);
-      toDosCopy.splice(destination?.index, 0, draggableId);
-      // console.log(toDosCopy);
-      return toDosCopy;
-    });
+    //   toDosCopy.splice(source.index, 1);
+    //   toDosCopy.splice(destination?.index, 0, draggableId);
+    //   return toDosCopy;
+    // });
   };
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Wrapper>
         <Boards>
-          <Droppable droppableId="one">
-            {(magic) => (
-              <Board ref={magic.innerRef} {...magic.droppableProps}>
-                {toDos.map((toDo, index) => (
-                  <DraggableCard key={toDo} index={index} toDo={toDo} />
-                ))}
-                {magic.placeholder}
-              </Board>
-            )}
-          </Droppable>
+          {Object.keys(toDos).map((boardId) => (
+            <Board boardId={boardId} key={boardId} toDos={toDos[boardId]} />
+          ))}
         </Boards>
       </Wrapper>
     </DragDropContext>
--- src/atom.tsx
+++ src/atom.tsx
@@ -1,6 +1,14 @@
-import { atom, selector } from "recoil";
+import { atom } from "recoil";
 
-export const toDoState = atom({
+interface ITodoState {
+  [key: string]: string[];
+}
+
+export const toDoState = atom<ITodoState>({
   key: "toDo",
-  default: ["a", "b", "c", "d", "e", "f"],
+  default: {
+    to_do: ["a", "b"],
+    doing: ["c", "d", "e"],
+    done: ["f"],
+  },
 });
